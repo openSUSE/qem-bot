75b2af1304d2efdd0709eb926d4979dac6a7188f
Ondřej Súkup <osukup@suse.com>
2021-09-10 19:08:27 +0200

    Remove defaul value for 'store_true'
    Argparse actions 'store_true' and 'store_false' have implicit
    defaults, and destinantion is generated by long arg so not need
    specify same destination

--- openqabot/args.py
+++ openqabot/args.py
@@ -104,8 +104,6 @@ def get_parser():
         "--ignore-onetime",
         action="store_true",
         help="Ignore onetime and schedule those test runs",
-        default=False,
-        dest="ignore_onetime",
     )
     cmdfull.set_defaults(func=do_full_schedule)
 
@@ -118,8 +116,6 @@ def get_parser():
         "--ignore-onetime",
         action="store_true",
         help="Ignore onetime and schedule those test runs",
-        default=False,
-        dest="ignore_onetime",
     )
     cmdinc.set_defaults(func=do_incident_schedule)
 
@@ -131,8 +127,6 @@ def get_parser():
         "--ignore-onetime",
         action="store_true",
         help="Ignore onetime and schedule those test runs",
-        default=False,
-        dest="ignore_onetime",
     )
     cmdupd.set_defaults(func=do_aggregate_schedule)
 
