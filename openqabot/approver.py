openSUSE/qem-bot

commit 9bf51d076168c002d7b2b8d31e18f77b76a4b01e

Clarify approval message
A request can not be approved by a dashboard, better only based on a
dashboard or data that is accessible over the dashboard. Also, we can
provide a link to an actual place where additional information could be
retrieved by readers.

openqabot/approver.py
@@ -97,7 +97,7 @@ class Approver:
     @staticmethod
     def osc_approve(inc: IncReq) -> None:
 
-        msg = "Request accepted for 'qam-openqa' by qam-dashboard"
+        msg = "Request accepted for 'qam-openqa' based on data in" + QEM_DASHBOARD
         logger.info(
             "Accepting review for SUSE:Maintenace:%s:%s" % (str(inc.inc), str(inc.req))
         )

commit e4e831e242ea47d96254616bdc3a1009db41ee09

Fix missing space in OSC message
Observed in https://build.suse.de/request/show/261850, there is a
missing space in the output string.

Fixes a problem introduced in 9bf51d0.

openqabot/approver.py
@@ -100,7 +100,7 @@ class Approver:
     @staticmethod
     def osc_approve(inc: IncReq) -> None:
 
-        msg = "Request accepted for 'qam-openqa' based on data in" + QEM_DASHBOARD
+        msg = "Request accepted for 'qam-openqa' based on data in " + QEM_DASHBOARD
         logger.info(
             "Accepting review for SUSE:Maintenace:%s:%s" % (str(inc.inc), str(inc.req))
         )