894c65a5c9381717e934fe1ec0d947c7eb4aa51e
Oliver Kurz <okurz@suse.de>
2022-04-21 13:42:07 +0200

    Add github CI instructions

--- tests/test_openqabot.py
+++ tests/test_openqabot.py
@@ -3,10 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 import pytest
-import os
 import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from openqabot.main import main  # SUT
 
