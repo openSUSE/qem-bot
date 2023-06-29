ad3c62d0f0230b69fdfdf572ab735fc5b5e85559
Anton Smorodskyi <asmorodskyi@suse.de>
2023-06-29 13:49:53 +0200

    Teach pc_helper_online to process metadata
    Initial version was doing only two function calls with dummy data
    This time we taking same folder with metadat as bot-ng. But instead
    of going through all actions done by bot-ng we concentrating on two variables
    which matter for Public Cloud. PUBLIC_CLOUD_TOOLS_IMAGE_QUERY and PUBLIC_CLOUD_PINT_QUERY

--- pc_helper_online.py
+++ pc_helper_online.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python3
 # Copyright SUSE LLC
 # SPDX-License-Identifier: MIT
+from argparse import ArgumentParser
 from pathlib import Path
+from ruamel.yaml import YAML  # type: ignore
 from openqabot.utils import create_logger
-from openqabot.loader.config import load_metadata
-from openqabot.types.incident import Incident
+from openqabot.pc_helper import apply_pc_tools_image, apply_publiccloud_pint_image
 
 
 def main():
     """
     This code is used only for testing purpose.
     Allowing to prove that Public Cloud related logic is actually working without executing
-    a lot of code which is unrelated to pc_helper
+    a lot of code which is unrelated to pc_helper.
+    As input it getting same folder as bot-ng but processing only two variables which are related to Public Cloud
     """
     log = create_logger("pc_helper_online")
-    products = load_metadata(Path('/home/asmorodskyi/source/metadata/bot-ng/'),False,True, {})
-    incidents = []
-    incidents.append(Incident({'approved': False, 'channels': ['SUSE:Updates:OpenStack-Cloud:9:x86_64'], 'emu': False, 'inReview': False, 'inReviewQAM': False, 'isActive': True, 'number': 17958, 'packages': ['ndctl'], 'project': 'SUSE:Maintenance:17958', 'rr_number': None}))
-    for product in products:
-        log.error(f" Calling for {product}")
-        product(incidents,{"token": "DDDDD"}, "DDDDD", True)
+    parser = ArgumentParser(
+                    prog='pc_helper_online',
+                    description='Dummy code to test functionality related to pc_helper code')
+    parser.add_argument(
+        "-c",
+        "--configs",
+        type=Path,
+        default=Path("/etc/openqabot"),
+        help="Directory with openqabot configuration metadata",
+    )
+    args = parser.parse_args()
+    loader = YAML(typ="safe")
+    log.info(f"Parsing configuration files from {args.configs}")
+    for p in Path(args.configs).glob("*.yml"):
+        try:
+            data = loader.load(p)
+            log.info(f"Processing {p}")
+            if "settings" in data:
+                settings = data["settings"]
+                if "PUBLIC_CLOUD_TOOLS_IMAGE_QUERY" in settings:
+                    apply_pc_tools_image(settings)
+                    if not settings.get("PUBLIC_CLOUD_TOOLS_IMAGE_BASE", False):
+                        log.error(f"Failed to get PUBLIC_CLOUD_TOOLS_IMAGE_BASE from {data}")
+                if "PUBLIC_CLOUD_PINT_QUERY" in settings:
+                    apply_publiccloud_pint_image(settings)
+                    if not settings.get("PUBLIC_CLOUD_IMAGE_ID", False):
+                        log.error(f"Failed to get PUBLIC_CLOUD_IMAGE_ID from {data}")
+        except Exception as e:
+            log.exception(e)
+            continue
 
 
 
