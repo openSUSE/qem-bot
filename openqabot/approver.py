5fc890c5b8f5726106ff7071f0222bc1ede758e9
Michael Grifalconi <mgrifalconi@suse.com>
2024-04-30 10:01:07 +0200

    improve build date parsing log

--- openqabot/approver.py
+++ openqabot/approver.py
@@ -168,7 +168,7 @@ class Approver:
         try:
             current_build_date = datetime.strptime(current_build, "%Y%m%d")
         except (ValueError, TypeError):
-            log.info("Could not parse build date %s", current_build)
+            log.info("Could not parse build date %s. Won't try to look at older jobs for approval.", current_build)
             return False
 
         # Use at most X days old build. Don't go back in time too much to reduce risk of using invalid tests
@@ -184,7 +184,7 @@ class Approver:
             try:
                 job_build_date = datetime.strptime(job_build, "%Y%m%d")
             except (ValueError, TypeError):
-                log.info("Could not parse build date %s", current_build)
+                log.info("Could not parse build date %s. Won't consider this job as alternative for approval.", current_build)
                 continue
 
             # Check the job is not too old
