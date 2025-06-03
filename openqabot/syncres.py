f3233cefc1bcbf936f62471a0808f7ada2e33284
Marius Kittler <mkittler@suse.de>
2025-06-03 13:52:17 +0200

    Allow considering development groups
    This makes it easier to demo pipelines in a non-production environment.
    
    Related ticket: https://progress.opensuse.org/issues/180812

--- openqabot/syncres.py
+++ openqabot/syncres.py
@@ -10,6 +10,7 @@ from .loader.qem import post_job
 from .openqa import openQAInterface
 from .types import Data
 from .utils import normalize_results
+from . import ALLOW_DEVELOPMENT_GROUPS
 
 log = getLogger("bot.syncres")
 
@@ -45,7 +46,7 @@ class SyncRes:
         return ret
 
     def _is_in_devel_group(self, data: Data) -> bool:
-        return (
+        return not ALLOW_DEVELOPMENT_GROUPS and (
             "Devel" in data["group"]
             or "Test" in data["group"]
             or self.client.is_devel_group(data["group_id"])
