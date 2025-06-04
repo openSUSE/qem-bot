openSUSE/qem-bot

commit f85ca71b13352cf53826d02ff3866fd2e5c6157b

Avoid hardcoding several URLs

openqabot/__init__.py
@@ -5,9 +5,11 @@ import os
 
 # Used configuration parameters, e.g. api url's
 QEM_DASHBOARD = os.environ.get("QEM_DASHBOARD_URL", "http://dashboard.qam.suse.de/")
-SMELT = os.environ.get("SMELT_URL", "https://smelt.suse.de/graphql")
+SMELT_URL = os.environ.get("SMELT_URL", "https://smelt.suse.de/graphql")
+SMELT = SMELT_URL + "/graphql"
 GITEA = os.environ.get("GITEA_URL", "https://src.suse.de")
 OBS_URL = os.environ.get("OBS_URL", "https://api.suse.de")
+OBS_DOWNLOAD_URL = os.environ.get("OBS_DOWNLOAD_URL", "http://download.suse.de/ibs")
 OBS_MAINT_PRJ = "SUSE:Maintenance"
 OBS_GROUP = "qam-openqa"
 DEVELOPMENT_PARENT_GROUP_ID = 9

openqabot/loader/repohash.py
@@ -8,6 +8,7 @@ from xml.etree import ElementTree as ET
 from requests import ConnectionError, HTTPError
 from requests.exceptions import RetryError
 
+from .. import OBS_DOWNLOAD_URL
 from ..errors import NoRepoFoundError
 from ..utils import retry5 as requests
 
@@ -21,13 +22,12 @@ def get_max_revision(
 ) -> int:
     max_rev = 0
 
-    url_start = "http://download.suse.de/ibs"
-    url_base = f"{url_start}/{project.replace(':',':/')}"
+    url_base = f"{OBS_DOWNLOAD_URL}/{project.replace(':',':/')}"
 
     for repo in repos:
         # handle URLs for SLFO specifically
         if project == "SLFO":
-            url = f"{url_start}/{repo[0].replace(':',':/')}:/{repo[1].replace(':',':/')}/standard/repodata/repomd.xml"
+            url = f"{OBS_DOWNLOAD_URL}/{repo[0].replace(':',':/')}:/{repo[1].replace(':',':/')}/standard/repodata/repomd.xml"
         # openSUSE and SLE incidents have different handling of architecture
         elif repo[0].startswith("openSUSE"):
             url = f"{url_base}/SUSE_Updates_{repo[0]}_{repo[1]}/repodata/repomd.xml"

openqabot/types/aggregate.py
@@ -7,7 +7,7 @@ from logging import getLogger
 from typing import Any, Dict, List, Optional
 
 from . import ProdVer, Repos
-from .. import DOWNLOAD_BASE, QEM_DASHBOARD
+from .. import DOWNLOAD_BASE, QEM_DASHBOARD, SMELT_URL
 from ..errors import NoTestIssues, SameBuildExists
 from ..loader.repohash import merge_repohash
 from ..pc_helper import apply_pc_tools_image, apply_publiccloud_pint_image
@@ -187,7 +187,7 @@ class Aggregate(BaseConf):
                 for inc in set(full_post["qem"]["incidents"])
             )
             full_post["openqa"]["__SMELT_INCIDENTS_URL"] = ",".join(
-                f"https://smelt.suse.de/incident/{inc}"
+                f"{SMELT_URL}/incident/{inc}"
                 for inc in set(full_post["qem"]["incidents"])
             )
 

openqabot/types/incidents.py
@@ -4,7 +4,7 @@ from logging import getLogger
 from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 from . import ProdVer, Repos
-from .. import QEM_DASHBOARD, GITEA
+from .. import QEM_DASHBOARD, GITEA, SMELT_URL
 from ..pc_helper import apply_pc_tools_image, apply_publiccloud_pint_image
 from ..utils import retry3 as requests
 from .baseconf import BaseConf
@@ -263,7 +263,7 @@ class Incidents(BaseConf):
         else:
             full_post["openqa"][
                 "__SMELT_INCIDENT_URL"
-            ] = f"https://smelt.suse.de/incident/{inc.id}"
+            ] = f"{SMELT_URL}/incident/{inc.id}"
         full_post["openqa"][
             "__DASHBOARD_INCIDENT_URL"
         ] = f"{QEM_DASHBOARD}incident/{inc.id}"

tests/test_smeltsync.py
@@ -7,7 +7,7 @@ import responses
 
 import openqabot.smeltsync
 from openqabot.smeltsync import SMELTSync
-from openqabot import QEM_DASHBOARD
+from openqabot import QEM_DASHBOARD, SMELT
 
 # Fake Namespace for SyncRes initialization
 _namespace = namedtuple("Namespace", ("dry", "token", "retry"))
@@ -17,7 +17,7 @@ _namespace = namedtuple("Namespace", ("dry", "token", "retry"))
 def fake_smelt_api(request):
     responses.add(
         responses.GET,
-        re.compile(r"https://smelt.suse.de/graphql\?query=.*"),
+        re.compile(SMELT + r"\?query=.*"),
         json={
             "data": {
                 "incidents": {

commit 1073b6f43b5a170c72462f53963015a593140d32

Fix default of SMELT_URL regression from f85ca71

openqabot/__init__.py
@@ -5,7 +5,7 @@ import os
 
 # Used configuration parameters, e.g. api url's
 QEM_DASHBOARD = os.environ.get("QEM_DASHBOARD_URL", "http://dashboard.qam.suse.de/")
-SMELT_URL = os.environ.get("SMELT_URL", "https://smelt.suse.de/graphql")
+SMELT_URL = os.environ.get("SMELT_URL", "https://smelt.suse.de")
 SMELT = SMELT_URL + "/graphql"
 GITEA = os.environ.get("GITEA_URL", "https://src.suse.de")
 OBS_URL = os.environ.get("OBS_URL", "https://api.suse.de")