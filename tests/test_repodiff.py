6eb4ebf1abedd7ff8c367da5337dce849fcb3693
Marius Kittler <mkittler@suse.de>
2025-11-12 17:44:36 +0100

    Add tests for computing repo diff
    Related ticket: https://progress.opensuse.org/issues/190542

---
+++ tests/test_repodiff.py
@@ -0,0 +1,22 @@
+# Copyright SUSE LLC
+# SPDX-License-Identifier: MIT
+import json
+from collections import namedtuple
+
+import pytest
+
+from openqabot.repodiff import RepoDiff
+
+_namespace = namedtuple("Namespace", ("dry", "fake_data", "repo_a", "repo_b"))
+
+
+def test_repodiff(capsys: pytest.CaptureFixture[str]) -> None:
+    RepoDiff(_namespace(True, True, "OBS:PROJECT:PUBLISH_product", "OBS:PROJECT:TEST_product"))()
+    res = json.loads(capsys.readouterr().out)
+    assert set(res.keys()) == {"aarch64", "ppc64le", "noarch"}
+
+
+def test_repodiff_compression(capsys: pytest.CaptureFixture[str]) -> None:
+    RepoDiff(_namespace(True, True, "OBS:PROJECT:PUBLISH_product_zst", "OBS:PROJECT:TEST_product_gz"))()
+    res = json.loads(capsys.readouterr().out)
+    assert set(res.keys()) == {"aarch64", "ppc64le", "noarch"}
