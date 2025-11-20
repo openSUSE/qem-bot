d4017c063517499c35d2a247f9bb0f8a3b3f7f00
Oliver Kurz <okurz@suse.de>
2025-11-20 18:58:48 +0100

    types/aggregate: Extract method 'get_repo_url'

--- openqabot/types/aggregate.py
+++ openqabot/types/aggregate.py
@@ -95,13 +95,15 @@ class Aggregate(BaseConf):
 
         for issue, incs in test_incidents.items():
             tmpl = issue.replace("ISSUES", "REPOS")
-            test_repos[tmpl].extend([
-                f"{DOWNLOAD_MAINTENANCE}{inc}/SUSE_Updates_{self.test_issues[issue].product}_{self.test_issues[issue].version}/"
-                if self.test_issues[issue].product.startswith("openSUSE")
-                else f"{DOWNLOAD_MAINTENANCE}{inc}/SUSE_Updates_{self.test_issues[issue].product}_{self.test_issues[issue].version}_{issues_arch}/"
-                for inc in incs
-            ])
+            test_repos[tmpl].extend([self._get_repo_url(inc, issue, issues_arch) for inc in incs])
         return test_incidents, test_repos
+
+    def _get_repo_url(self, inc: Incident, issue: str, issues_arch: str) -> str:
+        product = self.test_issues[issue].product
+        version = self.test_issues[issue].version
+        if product.startswith("openSUSE"):
+            return f"{DOWNLOAD_MAINTENANCE}{inc}/SUSE_Updates_{product}_{version}/"
+        return f"{DOWNLOAD_MAINTENANCE}{inc}/SUSE_Updates_{product}_{version}_{issues_arch}/"
 
     def _create_full_post(
         self,
