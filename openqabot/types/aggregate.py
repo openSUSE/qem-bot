b27e5c668d6395d565b54e024ca8fbcfc5891be8
Oliver Kurz <okurz@suse.de>
2025-11-20 18:33:08 +0100

    types/aggregate: Extract method from __call__ to fix C901

--- openqabot/types/aggregate.py
+++ openqabot/types/aggregate.py
@@ -6,7 +6,7 @@ import datetime
 from collections import defaultdict
 from itertools import chain
 from logging import getLogger
-from typing import Any
+from typing import Any, NamedTuple
 
 from openqabot import DEPRIORITIZE_LIMIT, DOWNLOAD_MAINTENANCE, QEM_DASHBOARD, SMELT_URL
 from openqabot.dashboard import get_json
@@ -20,6 +20,13 @@ from .baseconf import BaseConf
 from .incident import Incident
 
 log = getLogger("bot.types.aggregate")
+
+
+class _PostData(NamedTuple):
+    test_incidents: defaultdict[list]
+    test_repos: defaultdict[list]
+    repohash: str
+    build: str
 
 
 class Aggregate(BaseConf):
@@ -95,7 +102,70 @@ class Aggregate(BaseConf):
             ])
         return test_incidents, test_repos
 
-    def __call__(  # noqa: C901
+    def _create_full_post(
+        self,
+        arch: str,
+        data: _PostData,
+        ci_url: str | None,
+    ) -> dict[str, Any] | None:
+        full_post: dict[str, Any] = {}
+        full_post["openqa"] = {}
+        full_post["qem"] = {}
+        full_post["qem"]["incidents"] = []
+        full_post["qem"]["settings"] = {}
+        full_post["api"] = "api/update_settings"
+        if ci_url:
+            full_post["openqa"]["__CI_JOB_URL"] = ci_url
+
+        full_post["openqa"]["REPOHASH"] = data.repohash
+        full_post["openqa"]["BUILD"] = data.build
+
+        settings = self.settings.copy()
+
+        if "PUBLIC_CLOUD_TOOLS_IMAGE_QUERY" in settings:
+            settings = apply_pc_tools_image(settings)
+            if not settings.get("PUBLIC_CLOUD_TOOLS_IMAGE_BASE", False):
+                return None
+
+        if "PUBLIC_CLOUD_PINT_QUERY" in settings:
+            settings = apply_publiccloud_pint_image(settings)
+            if not settings.get("PUBLIC_CLOUD_IMAGE_ID", False):
+                return None
+
+        full_post["openqa"].update(settings)
+        full_post["openqa"]["FLAVOR"] = self.flavor
+        full_post["openqa"]["ARCH"] = arch
+        full_post["openqa"]["_DEPRIORITIZEBUILD"] = 1
+
+        if DEPRIORITIZE_LIMIT is not None:
+            full_post["openqa"]["_DEPRIORITIZE_LIMIT"] = DEPRIORITIZE_LIMIT
+
+        for template, issues in data.test_incidents.items():
+            full_post["openqa"][template] = ",".join(str(x) for x in issues)
+            full_post["qem"]["incidents"] += issues
+        for template, issues in data.test_repos.items():
+            full_post["openqa"][template] = ",".join(issues)
+
+        full_post["qem"]["incidents"] = [str(inc) for inc in set(full_post["qem"]["incidents"])]
+        if not full_post["qem"]["incidents"]:
+            return None
+
+        full_post["openqa"]["__DASHBOARD_INCIDENTS_URL"] = ",".join(
+            f"{QEM_DASHBOARD}incident/{inc}" for inc in full_post["qem"]["incidents"]
+        )
+        full_post["openqa"]["__SMELT_INCIDENTS_URL"] = ",".join(
+            f"{SMELT_URL}/incident/{inc}" for inc in full_post["qem"]["incidents"]
+        )
+
+        full_post["qem"]["settings"] = full_post["openqa"]
+        full_post["qem"]["repohash"] = full_post["openqa"]["REPOHASH"]
+        full_post["qem"]["build"] = full_post["openqa"]["BUILD"]
+        full_post["qem"]["arch"] = full_post["openqa"]["ARCH"]
+        full_post["qem"]["product"] = self.product
+
+        return full_post
+
+    def __call__(
         self,
         incidents: list[Incident],
         token: dict[str, str],
@@ -106,22 +176,13 @@ class Aggregate(BaseConf):
         ret = []
 
         for arch in self.archs:
-            full_post: dict[str, Any] = {}
-            full_post["openqa"] = {}
-            full_post["qem"] = {}
-            full_post["qem"]["incidents"] = []
-            full_post["qem"]["settings"] = {}
-            full_post["api"] = "api/update_settings"
-            if ci_url:
-                full_post["openqa"]["__CI_JOB_URL"] = ci_url
-
             # Temporary workaround for applying the correct architecture on jobs, which use a helper VM
             issues_arch = self.settings.get("TEST_ISSUES_ARCH", arch)
 
             valid_incidents = self._filter_incidents(incidents)
             test_incidents, test_repos = self._get_test_incidents_and_repos(valid_incidents, issues_arch)
 
-            full_post["openqa"]["REPOHASH"] = merge_repohash(
+            repohash = merge_repohash(
                 sorted({str(inc) for inc in chain.from_iterable(test_incidents.values())}),
             )
 
@@ -139,8 +200,8 @@ class Aggregate(BaseConf):
             old_build = old_jobs[0].get("build", "") if old_jobs else ""
 
             try:
-                full_post["openqa"]["BUILD"] = self.get_buildnr(
-                    full_post["openqa"]["REPOHASH"],
+                build = self.get_buildnr(
+                    repohash,
                     old_repohash,
                     old_build,
                 )
@@ -152,56 +213,16 @@ class Aggregate(BaseConf):
                 )
                 continue
 
-            if not ignore_onetime and (self.onetime and full_post["openqa"]["BUILD"].split("-")[-1] != "1"):
+            if not ignore_onetime and (self.onetime and build.split("-")[-1] != "1"):
                 continue
 
-            settings = self.settings.copy()
-
-            # if set, we use this query to detect latest public cloud tools image which used for running
-            # all public cloud related tests in openQA
-            if "PUBLIC_CLOUD_TOOLS_IMAGE_QUERY" in settings:
-                settings = apply_pc_tools_image(settings)
-                if not settings.get("PUBLIC_CLOUD_TOOLS_IMAGE_BASE", False):
-                    continue
-
-            # parse Public-Cloud pint query if present
-            if "PUBLIC_CLOUD_PINT_QUERY" in settings:
-                settings = apply_publiccloud_pint_image(settings)
-                if not settings.get("PUBLIC_CLOUD_IMAGE_ID", False):
-                    continue
-
-            full_post["openqa"].update(settings)
-            full_post["openqa"]["FLAVOR"] = self.flavor
-            full_post["openqa"]["ARCH"] = arch
-            full_post["openqa"]["_DEPRIORITIZEBUILD"] = 1
-
-            if DEPRIORITIZE_LIMIT is not None:
-                full_post["openqa"]["_DEPRIORITIZE_LIMIT"] = DEPRIORITIZE_LIMIT
-
-            for template, issues in test_incidents.items():
-                full_post["openqa"][template] = ",".join(str(x) for x in issues)
-                full_post["qem"]["incidents"] += issues
-            for template, issues in test_repos.items():
-                full_post["openqa"][template] = ",".join(issues)
-
-            full_post["qem"]["incidents"] = [str(inc) for inc in set(full_post["qem"]["incidents"])]
-            if not full_post["qem"]["incidents"]:
-                continue
-
-            full_post["openqa"]["__DASHBOARD_INCIDENTS_URL"] = ",".join(
-                f"{QEM_DASHBOARD}incident/{inc}" for inc in set(full_post["qem"]["incidents"])
-            )
-            full_post["openqa"]["__SMELT_INCIDENTS_URL"] = ",".join(
-                f"{SMELT_URL}/incident/{inc}" for inc in set(full_post["qem"]["incidents"])
+            full_post = self._create_full_post(
+                arch,
+                _PostData(test_incidents, test_repos, repohash, build),
+                ci_url,
             )
 
-            full_post["qem"]["settings"] = full_post["openqa"]
-            full_post["qem"]["repohash"] = full_post["openqa"]["REPOHASH"]
-            full_post["qem"]["build"] = full_post["openqa"]["BUILD"]
-            full_post["qem"]["arch"] = full_post["openqa"]["ARCH"]
-            full_post["qem"]["product"] = self.product
-
-            # add to ret
-            ret.append(full_post)
+            if full_post:
+                ret.append(full_post)
 
         return ret
