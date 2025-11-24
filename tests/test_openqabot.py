2eafe93e82d7b7152124853bbe4ffb46c757304c
Oliver Kurz <okurz@suse.de>
2025-11-24 21:49:23 +0100

    feat(main): Add test coverage for 'no_args_prints_helps'

--- tests/test_openqabot.py
+++ tests/test_openqabot.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import sys
+from unittest.mock import patch
 
 import pytest
 
@@ -9,6 +10,14 @@ from openqabot.main import main  # SUT
 
 
 def test_help() -> None:
-    sys.argv += ["--help"]
-    with pytest.raises(SystemExit):
+    with patch.object(sys, "argv", ["--help"]), pytest.raises(SystemExit):
+        main()
+
+
+def test_no_args_prints_help() -> None:
+    with (
+        patch.object(sys, "argv", []),
+        patch("openqabot.args.ArgumentParser.print_help"),
+        pytest.raises(SystemExit),
+    ):
         main()
