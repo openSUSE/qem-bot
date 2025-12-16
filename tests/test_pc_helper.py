8f0bd90a5da42d34ecb4173eb38b14524c708fbd
Oliver Kurz <okurz@suse.de>
2025-12-16 20:24:03 +0100

    refactor(tests/pc_helper): Use pytest-mock consistently instead of patch context manager

--- tests/test_pc_helper.py
+++ tests/test_pc_helper.py
@@ -1,9 +1,9 @@
 # Copyright SUSE LLC
 # SPDX-License-Identifier: MIT
 import re
-from unittest.mock import patch
 
 import pytest
+from pytest_mock import MockerFixture
 
 import openqabot.pc_helper
 import responses
@@ -15,104 +15,96 @@ from openqabot.pc_helper import (
 )
 
 
-def test_apply_pc_tools_image(caplog: pytest.LogCaptureFixture) -> None:
+def test_apply_pc_tools_image(mocker: MockerFixture, caplog: pytest.LogCaptureFixture) -> None:
     known_return = "test"
     settings = {"PUBLIC_CLOUD_TOOLS_IMAGE_QUERY": "test"}
-    with patch("openqabot.pc_helper.get_latest_tools_image", return_value=known_return):
-        apply_pc_tools_image(settings)
+    mocker.patch("openqabot.pc_helper.get_latest_tools_image", return_value=known_return)
+    apply_pc_tools_image(settings)
     assert "PUBLIC_CLOUD_TOOLS_IMAGE_BASE" in settings
     assert settings["PUBLIC_CLOUD_TOOLS_IMAGE_BASE"] == known_return
     assert "PUBLIC_CLOUD_TOOLS_IMAGE_QUERY" not in settings
-    with patch("openqabot.pc_helper.get_latest_tools_image"):
-        apply_pc_tools_image(settings)
+    mocker.patch("openqabot.pc_helper.get_latest_tools_image")
+    apply_pc_tools_image(settings)
     settings = {"PUBLIC_CLOUD_TOOLS_IMAGE_QUERY": "test"}
-    with patch("openqabot.pc_helper.get_latest_tools_image", side_effect=BaseException):
-        apply_pc_tools_image(settings)
+    mocker.patch("openqabot.pc_helper.get_latest_tools_image", side_effect=BaseException)
+    apply_pc_tools_image(settings)
     assert "PUBLIC_CLOUD_TOOLS_IMAGE_BASE handling failed" in caplog.text
 
 
-def test_pint_query_uses_cache() -> None:
-    with patch("openqabot.pc_helper.retried_requests.get") as get_mock:
-        for _ in range(1, 3):
-            openqabot.pc_helper.pint_query("foo")
-        get_mock.assert_called_once()
+def test_pint_query_uses_cache(mocker: MockerFixture) -> None:
+    get_mock = mocker.patch("openqabot.pc_helper.retried_requests.get")
+    for _ in range(1, 3):
+        openqabot.pc_helper.pint_query("foo")
+    get_mock.assert_called_once()
 
 
-def test_apply_publiccloud_pint_image() -> None:
-    with (
-        patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []}),
-        patch(
-            "openqabot.pc_helper.get_recent_pint_image",
-            side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
-        ),
-    ):
-        settings = {}
-        apply_publiccloud_pint_image(settings)
-        assert settings["PUBLIC_CLOUD_IMAGE_ID"] is None
-        assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
-        assert "PUBLIC_CLOUD_PINT_NAME" not in settings
-        assert "PUBLIC_CLOUD_PINT_REGION" not in settings
-        assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
-        assert "PUBLIC_CLOUD_REGION" not in settings
+def test_apply_publiccloud_pint_image(mocker: MockerFixture) -> None:
+    mocker.patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []})
+    mocker.patch(
+        "openqabot.pc_helper.get_recent_pint_image",
+        side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
+    )
+    settings = {}
+    apply_publiccloud_pint_image(settings)
+    assert settings["PUBLIC_CLOUD_IMAGE_ID"] is None
+    assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
+    assert "PUBLIC_CLOUD_PINT_NAME" not in settings
+    assert "PUBLIC_CLOUD_PINT_REGION" not in settings
+    assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
+    assert "PUBLIC_CLOUD_REGION" not in settings
 
-    with (
-        patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []}),
-        patch(
-            "openqabot.pc_helper.get_recent_pint_image",
-            side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
-        ),
-    ):
-        settings = {
-            "PUBLIC_CLOUD_PINT_QUERY": "test",
-            "PUBLIC_CLOUD_PINT_NAME": "test",
-            "PUBLIC_CLOUD_PINT_FIELD": "image_id",
-        }
-        apply_publiccloud_pint_image(settings)
-        assert settings["PUBLIC_CLOUD_IMAGE_ID"] == "111"
-        assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
-        assert "PUBLIC_CLOUD_PINT_NAME" not in settings
-        assert "PUBLIC_CLOUD_PINT_REGION" not in settings
-        assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
-        assert "PUBLIC_CLOUD_REGION" not in settings
+    mocker.patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []})
+    mocker.patch(
+        "openqabot.pc_helper.get_recent_pint_image",
+        side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
+    )
+    settings = {
+        "PUBLIC_CLOUD_PINT_QUERY": "test",
+        "PUBLIC_CLOUD_PINT_NAME": "test",
+        "PUBLIC_CLOUD_PINT_FIELD": "image_id",
+    }
+    apply_publiccloud_pint_image(settings)
+    assert settings["PUBLIC_CLOUD_IMAGE_ID"] == "111"
+    assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
+    assert "PUBLIC_CLOUD_PINT_NAME" not in settings
+    assert "PUBLIC_CLOUD_PINT_REGION" not in settings
+    assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
+    assert "PUBLIC_CLOUD_REGION" not in settings
 
-    with (
-        patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []}),
-        patch(
-            "openqabot.pc_helper.get_recent_pint_image",
-            side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
-        ),
-    ):
-        settings = {
-            "PUBLIC_CLOUD_PINT_QUERY": "test",
-            "PUBLIC_CLOUD_PINT_NAME": "test",
-            "PUBLIC_CLOUD_PINT_FIELD": "image_id",
-            "PUBLIC_CLOUD_PINT_REGION": "south",
-        }
-        apply_publiccloud_pint_image(settings)
-        assert settings["PUBLIC_CLOUD_IMAGE_ID"] == "111"
-        assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
-        assert settings["PUBLIC_CLOUD_REGION"] == "south"
-        assert "PUBLIC_CLOUD_PINT_NAME" not in settings
-        assert "PUBLIC_CLOUD_PINT_REGION" not in settings
-        assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
+    mocker.patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []})
+    mocker.patch(
+        "openqabot.pc_helper.get_recent_pint_image",
+        side_effect=lambda *_args, **_kwargs: {"name": "test", "state": "active", "image_id": "111"},
+    )
+    settings = {
+        "PUBLIC_CLOUD_PINT_QUERY": "test",
+        "PUBLIC_CLOUD_PINT_NAME": "test",
+        "PUBLIC_CLOUD_PINT_FIELD": "image_id",
+        "PUBLIC_CLOUD_PINT_REGION": "south",
+    }
+    apply_publiccloud_pint_image(settings)
+    assert settings["PUBLIC_CLOUD_IMAGE_ID"] == "111"
+    assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
+    assert settings["PUBLIC_CLOUD_REGION"] == "south"
+    assert "PUBLIC_CLOUD_PINT_NAME" not in settings
+    assert "PUBLIC_CLOUD_PINT_REGION" not in settings
+    assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
 
-    with (
-        patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []}),
-        patch("openqabot.pc_helper.get_recent_pint_image", side_effect=lambda *_args, **_kwargs: None),
-    ):
-        settings = {
-            "PUBLIC_CLOUD_PINT_QUERY": "test",
-            "PUBLIC_CLOUD_PINT_NAME": "test",
-            "PUBLIC_CLOUD_PINT_FIELD": "image_id",
-            "PUBLIC_CLOUD_PINT_REGION": "south",
-        }
-        apply_publiccloud_pint_image(settings)
-        assert settings["PUBLIC_CLOUD_IMAGE_ID"] is None
-        assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
-        assert settings["PUBLIC_CLOUD_REGION"] == "south"
-        assert "PUBLIC_CLOUD_PINT_NAME" not in settings
-        assert "PUBLIC_CLOUD_PINT_REGION" not in settings
-        assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
+    mocker.patch("openqabot.pc_helper.pint_query", side_effect=lambda *_args, **_kwargs: {"images": []})
+    mocker.patch("openqabot.pc_helper.get_recent_pint_image", side_effect=lambda *_args, **_kwargs: None)
+    settings = {
+        "PUBLIC_CLOUD_PINT_QUERY": "test",
+        "PUBLIC_CLOUD_PINT_NAME": "test",
+        "PUBLIC_CLOUD_PINT_FIELD": "image_id",
+        "PUBLIC_CLOUD_PINT_REGION": "south",
+    }
+    apply_publiccloud_pint_image(settings)
+    assert settings["PUBLIC_CLOUD_IMAGE_ID"] is None
+    assert "PUBLIC_CLOUD_PINT_QUERY" not in settings
+    assert settings["PUBLIC_CLOUD_REGION"] == "south"
+    assert "PUBLIC_CLOUD_PINT_NAME" not in settings
+    assert "PUBLIC_CLOUD_PINT_REGION" not in settings
+    assert "PUBLIC_CLOUD_PINT_FIELD" not in settings
 
 
 def test_get_recent_pint_image() -> None:
