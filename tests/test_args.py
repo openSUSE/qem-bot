79c24f77bf389a805e1821ab970215eae9c65166
Oliver Kurz <okurz@suse.de>
2025-12-17 09:38:11 +0100

    feat(tests/args): Add complete statement+branch coverage

--- tests/test_args.py
+++ tests/test_args.py
@@ -11,3 +11,82 @@ def test_do_full_schedule(mocker: MockerFixture) -> None:
     openqabot.args.do_full_schedule(args)
     bot.assert_called_once_with(args)
     assert not args.disable_aggregates
+
+
+def test_do_incident_schedule(mocker: MockerFixture) -> None:
+    bot = mocker.patch("openqabot.openqabot.OpenQABot")
+    args = mocker.Mock()
+    openqabot.args.do_incident_schedule(args)
+    bot.assert_called_once_with(args)
+    assert args.disable_aggregates
+
+
+def test_do_aggregate_schedule(mocker: MockerFixture) -> None:
+    bot = mocker.patch("openqabot.openqabot.OpenQABot")
+    args = mocker.Mock()
+    openqabot.args.do_aggregate_schedule(args)
+    bot.assert_called_once_with(args)
+    assert not args.disable_aggregates
+
+
+def test_do_sync_smelt(mocker: MockerFixture) -> None:
+    smeltsync = mocker.patch("openqabot.smeltsync.SMELTSync")
+    args = mocker.Mock()
+    openqabot.args.do_sync_smelt(args)
+    smeltsync.assert_called_once_with(args)
+
+
+def test_do_sync_gitea(mocker: MockerFixture) -> None:
+    giteasync = mocker.patch("openqabot.giteasync.GiteaSync")
+    args = mocker.Mock()
+    openqabot.args.do_sync_gitea(args)
+    giteasync.assert_called_once_with(args)
+
+
+def test_do_approve(mocker: MockerFixture) -> None:
+    approver = mocker.patch("openqabot.approver.Approver")
+    args = mocker.Mock()
+    openqabot.args.do_approve(args)
+    approver.assert_called_once_with(args)
+
+
+def test_do_comment(mocker: MockerFixture) -> None:
+    commenter = mocker.patch("openqabot.commenter.Commenter")
+    args = mocker.Mock()
+    openqabot.args.do_comment(args)
+    commenter.assert_called_once_with(args)
+
+
+def test_do_sync_inc_results(mocker: MockerFixture) -> None:
+    syncer = mocker.patch("openqabot.incsyncres.IncResultsSync")
+    args = mocker.Mock()
+    openqabot.args.do_sync_inc_results(args)
+    syncer.assert_called_once_with(args)
+
+
+def test_do_sync_aggregate_results(mocker: MockerFixture) -> None:
+    syncer = mocker.patch("openqabot.aggrsync.AggregateResultsSync")
+    args = mocker.Mock()
+    openqabot.args.do_sync_aggregate_results(args)
+    syncer.assert_called_once_with(args)
+
+
+def test_do_increment_approve(mocker: MockerFixture) -> None:
+    approve = mocker.patch("openqabot.incrementapprover.IncrementApprover")
+    args = mocker.Mock()
+    openqabot.args.do_increment_approve(args)
+    approve.assert_called_once_with(args)
+
+
+def test_do_repo_diff_computation(mocker: MockerFixture) -> None:
+    repo_diff = mocker.patch("openqabot.repodiff.RepoDiff")
+    args = mocker.Mock()
+    openqabot.args.do_repo_diff_computation(args)
+    repo_diff.assert_called_once_with(args)
+
+
+def test_do_amqp(mocker: MockerFixture) -> None:
+    amqp = mocker.patch("openqabot.amqp.AMQP")
+    args = mocker.Mock()
+    openqabot.args.do_amqp(args)
+    amqp.assert_called_once_with(args)
