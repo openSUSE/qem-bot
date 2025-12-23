a02afae773e71b7f24919a28bcf8d948298bbaea
Oliver Kurz <okurz@suse.de>
2025-12-23 12:16:00 +0100

    feat(tests/repohash): Extend test coverage for loader/repohash.py

--- tests/test_repohash.py
+++ tests/test_repohash.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from unittest.mock import patch
+from unittest.mock import ANY, patch
 
 import pytest
 import requests
@@ -158,3 +158,40 @@ def test_get_max_revision_slfo_product_not_in_obs_products(caplog: pytest.LogCap
 
 def test_merge_repohash() -> None:
     assert rp.merge_repohash(["a", "b", "c"]) == "c7e84e227cb118dbe1fa7d49b3e55fc3"
+
+
+@responses.activate
+def test_get_max_revision_slfo() -> None:
+    repos = [("SLFO-Module", "1.1.99")]
+    arch = "x86_64"
+    project = "SLFO"
+    product_version = "15.99"
+
+    with (
+        patch("openqabot.loader.repohash.gitea.get_product_name", return_value="SLES"),
+        patch("openqabot.loader.repohash.OBS_PRODUCTS", {"SLES"}),
+        patch(
+            "openqabot.loader.repohash.gitea.compute_repo_url",
+            return_value="http://download.suse.de/ibs/SLFO/repo/repodata/repomd.xml",
+        ) as mock_compute_url,
+    ):
+        responses.add(
+            responses.GET,
+            url="http://download.suse.de/ibs/SLFO/repo/repodata/repomd.xml",
+            body=BASE_XML % "123",
+        )
+
+        # Call with product_version
+        ret = rp.get_max_revision(repos, arch, project, product_version=product_version)
+        assert ret == 123
+        mock_compute_url.assert_called_with(ANY, "SLES", ("SLFO-Module", "1.1.99", product_version), arch)
+
+        # Call without product_version
+        ret = rp.get_max_revision(repos, arch, project)
+        assert ret == 123
+        mock_compute_url.assert_called_with(ANY, "SLES", ("SLFO-Module", "1.1.99"), arch)
+
+        # Call with product_name set
+        ret = rp.get_max_revision(repos, arch, project, product_name="SLES")
+        assert ret == 123
+        mock_compute_url.assert_called_with(ANY, "SLES", ("SLFO-Module", "1.1.99"), arch)
