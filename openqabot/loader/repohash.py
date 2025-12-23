88925579a10162fde0e2c0cbe2bf7723cfe98e14
Oliver Kurz <okurz@suse.de>
2025-12-23 19:57:27 +0100

    refactor(loader/repohash): Remove redundant blank lines

--- openqabot/loader/repohash.py
+++ openqabot/loader/repohash.py
@@ -72,8 +72,6 @@ def get_max_revision(
 
 def merge_repohash(hashes: list[str]) -> str:
     m = md5(b"start")  # noqa: S324 hashlib-insecure-hash-function
-
     for h in hashes:
         m.update(h.encode())
-
     return m.hexdigest()
