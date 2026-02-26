6ea6fe0bd2e89cad5462b51b89feb0d18e04860b
Ioannis Bonatakis <ybonatakis@suse.com>
2026-02-26 12:52:06 +0100

    test(submissions): test coverage for openSUSE submissions from Gitea
    Extends testing for all the work on
    related-issue: https://progress.opensuse.org/issues/189927
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/test_submissions_gitea.py
+++ tests/test_submissions_gitea.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: MIT
 """Test Submissions Gitea."""
 
+import pytest
+
 import responses
 from openqabot.types.baseconf import JobConfig
 from openqabot.types.submissions import Submissions
@@ -36,7 +38,7 @@ def _assert_gitea_settings(
         "DISTRI": distri,
         "FLAVOR": flavor,
         "INCIDENT_ID": sub_id,
-        "INCIDENT_REPO": f"{expected_repo}-{arch}/",
+        "INCIDENT_REPO": expected_repo,
         "REPOHASH": repo_hash,
         "VERSION": product_ver,
     }
@@ -47,16 +49,31 @@ def _assert_gitea_settings(
 
 
 @responses.activate
-def test_gitea_submissions() -> None:
+@pytest.mark.parametrize(
+    ("product", "version", "distri", "test_issue", "inc_repo"),
+    [
+        (
+            "openSUSE:Leap",
+            "1.1.99:PullRequest:166",
+            "opensuse",
+            "openSUSE/Leap:1.1.99",
+            "http://%REPO_MIRROR_HOST%/ibs/openSUSE:/Leap:/1.1.99:/PullRequest:/166",
+        ),
+        (
+            "SUSE:SLFO",
+            "1.1.99:PullRequest:166:SLES",
+            "sles",
+            "SLFO:1.1.99#15.99",
+            "http://%REPO_MIRROR_HOST%/ibs/SUSE:/SLFO:/1.1.99:/PullRequest:/166:/SLES/product/repo/SLES-15.99-{arch}/",
+        ),
+    ],
+)
+def test_gitea_submissions(product: str, version: str, distri: str, test_issue: str, inc_repo: str) -> None:
     # declare fields of Repos used in this test
-    product = "SUSE:SLFO"  # "product" is used to store the name of the codestream in Gitea-based submissions …
-    version = "1.1.99:PullRequest:166:SLES"  # … and version is the full project including the product
     archs = ["x86_64", "aarch64"]
     product_ver = "15.99"
-
-    # declare meta-data
-    settings = {"VERSION": product_ver, "DISTRI": "sles"}
-    issues = {"BASE_TEST_ISSUES": "SLFO:1.1.99#15.99"}
+    settings = {"VERSION": product_ver, "DISTRI": distri}
+    issues = {"BASE_TEST_ISSUES": test_issue}
     flavor = "AAA"
     test_config = {"FLAVOR": {flavor: {"archs": archs, "issues": issues}}}
 
@@ -68,12 +85,12 @@ def test_gitea_submissions() -> None:
     sub.revisions = {ArchVer(arch, product_ver): repo_hash for arch in archs}
 
     # compute openQA/dashboard settings for submission and check results
-    subs = Submissions(JobConfig("SLFO", None, None, settings, test_config), set())
+    subs = Submissions(JobConfig(product, None, None, settings, test_config), set())
     subs.singlearch = set()
-    expected_repo = "http://%REPO_MIRROR_HOST%/ibs/SUSE:/SLFO:/1.1.99:/PullRequest:/166:/SLES/product/repo/SLES-15.99"
     res = subs(submissions=[sub], token={}, ci_url="", ignore_onetime=False)
     assert len(res) == len(archs)
     for arch, result in zip(archs, res):
+        expected_repo = inc_repo.format(arch=arch)
         _assert_gitea_settings(
             result,
             arch=arch,
